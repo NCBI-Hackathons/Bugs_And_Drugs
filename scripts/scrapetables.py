ef1d1eff52f77cef05fa694b57fac30c3e6b6fcf
Matt Starr <matt@mattstarr.net>
2018-07-24 17:24:11 -0400

    add script to scrape drug resistance information from kegg site into tsv file

---
+++ scripts/scrapetables.py
@@ -0,0 +1,57 @@
+# Script for scraping drug resistance information into TSV file from KEGG, as API does not seem to return
+# this information, currently
+# requirements: beatifulsoup4
+
+#reference:  https://stackoverflow.com/questions/18966368/python-beautifulsoup-scrape-tables
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# the first page has 8 columns as opposed to 6, for the rest
+url = "https://www.genome.jp/kegg/annotation/br01553.html"
+page = urlopen(url)
+soup = BeautifulSoup(page, "html.parser")
+resistantTo = "beta-Lactamase"
+linecount = 0
+with open('keggtable1.tsv', 'w') as f:
+    for tr in soup.find_all('tr')[2:]:
+        tds = tr.find_all('td')
+        if len(tds) == 8:
+            linecount += 1
+            if linecount == 1:
+                resistance = "Resistance"
+            else:
+                resistance = resistantTo
+            f.write((tds[0].text + '\t' + tds[1].text + '\t' + tds[2].text + '\t' + tds[3].text + '\t' +
+                     tds[4].text + '\t' + tds[5].text + '\t' + tds[6].text + '\t' + tds[7].text + '\t' +
+                     resistance + '\n'))
+
+urlList = ["https://www.genome.jp/kegg/annotation/br01554.html",
+           "https://www.genome.jp/kegg/annotation/br01555.html",
+           "https://www.genome.jp/kegg/annotation/br01556.html",
+           "https://www.genome.jp/kegg/annotation/br01557.html"]
+filecount = 2
+for url in urlList:
+    linecount = 0
+    page = urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
+    if filecount == 2:
+        resistantTo = "Aminoglycoside"
+    if filecount == 3:
+        resistantTo = "Macrolide-Lincosamide-Streptogramin"
+    if filecount == 4:
+        resistantTo = "Tetracycline"
+    if filecount == 5:
+        resistantTo = "Other"  # need to change this later -- this page has multiple tables that we can not differentiate from for now
+    with open('keggtable' + str(filecount) + '.tsv', 'w') as f:
+        for tr in soup.find_all('tr')[2:]:
+            tds = tr.find_all('td')
+            if len(tds) == 7:
+                linecount += 1
+                if linecount == 1:
+                    resistance = "Resistance"
+                else:
+                    resistance = resistantTo
+                f.write((tds[0].text + '\t' + tds[1].text + '\t' + tds[2].text + '\t' +
+                         tds[3].text + '\t' + tds[4].text + '\t' + tds[5].text + '\t' +
+                         resistance + '\n'))
+    filecount += 1
